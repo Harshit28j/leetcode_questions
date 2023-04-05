443b89fb91b84fbc2b769b8b568f2d2ee71aa4f0
Harshit Jain <48647625+Harshit28j@users.noreply.github.com>
2023-04-05 18:30:05 +0530

    Create peakIndexInMountainArray.java

---
+++ Using Java/peakIndexInMountainArray.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) { // decreasing side of array
+                end = mid;
+            } else { // increasing side of array
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
